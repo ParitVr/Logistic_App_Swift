22841a09ee2a979cad7c90da50aa639d212aa882
ParitVr <97943731+ParitVr@users.noreply.github.com>
2022-03-08 17:30:58 +0700

    Parit

--- app/src/main/java/com/example/logisticappswift/fragments/DriverHomeFragment.kt
+++ app/src/main/java/com/example/logisticappswift/fragments/DriverHomeFragment.kt
@@ -5,61 +5,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.logisticappswift.R
 import com.example.logisticappswift.user_data
+import com.google.android.material.tabs.TabLayoutMediator
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_driver.*
+import kotlinx.android.synthetic.main.fragment_driver_home.*
 import kotlinx.android.synthetic.main.fragment_driver_home.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DriverHomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class DriverHomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+class DriverHomeFragment : Fragment() {
+
+    private val jobAccepted =DriverHomeAcceptedJobFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        //replace_fragment(jobAccepted)
+//
+//        driver_homepage_nav.setOnItemSelectedListener {
+//            when(it.itemId){
+//                R.id.driver_job_accepted_nav -> replace_fragment(jobAccepted)
+//                R.id.driver_job_history_nav ->  replace_fragment(jobAccepted)
+//            }
+//            true
+//        }
         val view = inflater.inflate(R.layout.fragment_driver_home, container, false)
         Picasso.get().load(user_data.profile_img_url).into(view.driver_home_profile_img)
         view.driver_home_username.text = user_data.username
         return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DriverHomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DriverHomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+//    private fun replace_fragment(fragment: Fragment){
+//        if(fragment != null){
+//            //post_list.clear()
+//            val transaction = parentFragmentManager.beginTransaction()
+//            transaction.replace(R.id.driver_home_fragment_container, fragment)
+//            transaction.commit()
+//        }
+//    }
 }
